ad55d81005e657517ed04fa092a4672ff454f31e
Matthias <Matthias@family-burger.de>
2016-12-01 21:34:09 +0100

    init

---
+++ windows Service/windows Service/Service1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace windows_Service
+{
+    public partial class MyWindowsServic : ServiceBase
+    {
+        public MyWindowsServic()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
